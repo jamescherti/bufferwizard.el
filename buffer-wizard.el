2a8b17294eae35f44f8963b941c6c4511c825906
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-11-28 23:05:08 -0500

    Add: buffer-wizard-rename-file

--- buffer-wizard.el
+++ buffer-wizard.el
@@ -28,7 +28,7 @@
 ;;; Code:
 
 (defgroup buffer-wizard nil
-  "Buffer wizard"
+  "Buffer wizard."
   :group 'buffer-wizard
   :prefix "buffer-wizard-"
   :link '(url-link
@@ -45,15 +45,101 @@ The message is formatted with the provided arguments ARGS."
 The message is formatted with the provided arguments ARGS."
   (apply #'message (concat "[buffer-wizard] Warning: " (car args)) (cdr args)))
 
-;;;###autoload
-(define-minor-mode buffer-wizard-mode
-  "Toggle `buffer-wizard-mode'."
-  :global t
-  :lighter " buffer-wizard"
-  :group 'buffer-wizard
-  (if buffer-wizard-mode
-      t
-    t))
+(defvar buffer-wizard-after-rename-functions nil
+  "List of functions to run after renaming.
+Each function takes one argument: the buffer being renamed.")
+
+(defun buffer-wizard--rename-all-buffer-names (old-filename new-filename)
+  "Update buffer names to reflect the renaming of a file.
+OLD-FILENAME and NEW-FILENAME are absolute paths as returned by `file-truename'.
+
+For all buffers associated with OLD-FILENAME, update the buffer names to use
+NEW-FILENAME.
+
+This includes indirect buffers whose names are derived from the old filename."
+  (let ((basename (file-name-nondirectory old-filename))
+        (new-basename (file-name-nondirectory new-filename)))
+    (dolist (buf (buffer-list))
+      (when (buffer-live-p buf)
+        (with-current-buffer buf
+          (let ((base-buffer (buffer-base-buffer)))
+            (when base-buffer
+              (let* ((base-buffer-file-name
+                      (let ((file-name (buffer-file-name base-buffer)))
+                        (when file-name
+                          (file-truename file-name)))))
+                (when (and base-buffer-file-name
+                           (string= base-buffer-file-name new-filename))
+                  (let ((indirect-buffer-name (buffer-name)))
+                    (let* ((new-buffer-name (concat new-basename
+                                                    (substring
+                                                     indirect-buffer-name
+                                                     (length basename)))))
+                      (when (string-prefix-p basename indirect-buffer-name)
+                        (when new-buffer-name
+                          (rename-buffer new-buffer-name))))))))))))))
+
+(defun buffer-wizard-rename-file ()
+  "Rename the current buffer and the file it is visiting.
+This command updates the file name on disk, adjusts the buffer name, and updates
+any indirect buffers or other buffers associated with the old file.
+
+Hooks in `buffer-wizard-after-rename-functions' are run after the renaming
+process."
+  (interactive)
+  (let* ((filename (let ((buffer-file-name (buffer-base-buffer)))
+                     (when buffer-file-name
+                       (file-truename buffer-file-name))))
+         (original-buffer (when filename
+                            (get-file-buffer filename))))
+    (unless filename
+      (error "The buffer with the name '%s' is not visiting a file"
+             (buffer-name)))
+
+    (unless (file-regular-p filename)
+      (error "The file '%s' cannot be found on the disk" filename))
+
+    (unless original-buffer
+      (error "Unable to find the buffer of: %s"
+             (buffer-name)))
+
+    (with-current-buffer original-buffer
+      (when (buffer-modified-p)
+        (let ((save-silently t))
+          (save-buffer)))
+
+      (let* ((basename (if filename
+                           (file-name-nondirectory filename)
+                         ""))
+             (new-basename (read-string "New name: " basename)))
+        (when (not (string= basename new-basename))
+          (let ((new-filename (expand-file-name new-basename
+                                                (file-name-directory filename))))
+            (if (vc-backend filename)
+                (progn
+                  ;; Rename the file using VC
+                  (vc-rename-file filename new-basename)
+                  (message "[VC RENAME] %s -> %s"
+                           filename (file-name-nondirectory new-filename)))
+              ;; Rename
+              (rename-file filename new-filename 1)
+              (message "[RENAME] %s -> %s"
+                       filename (file-name-nondirectory new-filename)))
+
+            (set-visited-file-name new-filename t t)
+
+            ;; Update all buffers pointing to the old file
+            ;; Broken
+            (buffer-wizard--rename-all-buffer-names filename
+                                                    new-filename)
+
+            ;; To make sure it is added to other plugins such as recentf
+            (run-hooks 'find-file-hook)
+
+            ;; (run-hook-with-args buffer-wizard-after-rename-functions
+            ;;                     (current-buffer))
+
+            ))))))
 
 (provide 'buffer-wizard)
 ;;; buffer-wizard.el ends here
