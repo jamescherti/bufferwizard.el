70105511840628c1c6c8477b0bd88b4b6c61ba42
James Cherti <60946298+jamescherti@users.noreply.github.com>
2025-01-21 15:19:21 -0500

    Update copyright year range

--- bufferwizard.el
+++ bufferwizard.el
@@ -1,6 +1,6 @@
 ;;; bufferwizard.el --- Buffer wizard  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024 James Cherti | https://www.jamescherti.com/contact/
+;; Copyright (C) 2024-2025 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
 ;; Version: 0.9.9
